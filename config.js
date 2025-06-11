2cbab15bd5a629e730c94a76033526c0e739d491
findus-jdk <findus.jdk@gmail.com>
2025-06-11 14:37:51 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,15 @@
+const companyConfig = {
+    companyName: "D&G BEAUTY CARE",
+    slogan: "Innovating for a better tomorrow",
+    logoUrl: "logo.jpeg",
+    contact: {
+        phone: "+919824771553",
+        address: "Shubhvihar apartment, Bhavnagar - Sidsar Rd, Kailash Dham Society, Bhavnagar, Gujarat 364002"
+    },
+    socialMedia: {
+        whatsapp: "https://wa.me/919824771553",
+        instagram: "https://www.instagram.com/d_g_beautycare/?utm_source=qr&igsh=MXgzdGZ4cnV2Z3I2aA%3D%3D",
+        googleReviews: "https://www.google.com/search?sca_esv=ea7b27f3c7f5bbf6&si=AMgyJEtREmoPL4P1I5IDCfuA8gybfVI2d5Uj7QMwYCZHKDZ-E5b2mVuRdBh5tbJqT8It1wEv7BJnIvZ-iISGjJsurYtHwSCPlflwQPREcYGrRW9rNalQfpK_VHD4fX5e2fpIvKeLcagt&q=D%26G+Beauty+care+Reviews&sa=X&ved=2ahUKEwjD7-GV8-aNAxWclK8BHdZBEzIQ0bkNegQIHxAE&biw=1440&bih=679&dpr=2#lrd=0x395f51a6222a2ac3:0xbdc1893f5f385a21,3,,,,",
+        map: "https://www.google.com/maps/place/D%26G+Beauty+care/@21.7294848,72.1371357,752m/data=!3m1!1e3!4m16!1m9!3m8!1s0x395f51a6222a2ac3:0xbdc1893f5f385a21!2sD%26G+Beauty+care!8m2!3d21.7294798!4d72.1397106!9m1!1b1!16s%2Fg%2F11jszph1kd!3m5!1s0x395f51a6222a2ac3:0xbdc1893f5f385a21!8m2!3d21.7294798!4d72.1397106!16s%2Fg%2F11jszph1kd?entry=ttu&g_ep=EgoyMDI1MDYwNC4wIKXMDSoASAFQAw%3D%3D"
+    }
+};
